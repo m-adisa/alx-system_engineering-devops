17206afb88873e75248123b7d4f44e97ff8add84
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-11-02 15:34:43 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""
-Working with the Reddit API
-"""
+""" a program that queries Reddit Api and returns
+the number of subscribers """
+
 import requests
 
-BASE = 'https://www.reddit.com'
-""" Reddit's base API URL """
+Base_url = 'https://www.reddit.com'
+
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0
-    """
+    """ a function that queries reddit's api"""
 
     api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0)',
-            'Gecko/20100101',
-            'Firefox/106.0'
-            ])
+         'Accept': 'application/json',
+         'user-agent': ' '.join([
+             'Mozilla/5.0 (Linux; Android 6.0;Nexus 5 Build/MRA58N)'
+             'AppleWebKit/537.36 (KHTML, like Gecko)'
+             'Chrome/106.0.0.0 Mobile Safari/537.36'
+         ])
         }
 
     res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE, subreddit),
-        headers = api_headers,
-        allow_redirects = False
-        )
-
+        '{}/r/{}/about/.json'.format(Base_url, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
     if res.status_code == 200:
         return res.json()['data']['subscribers']
     return 0
